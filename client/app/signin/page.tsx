101b655fb5b2345a73701d8262fe01c85160938f
Maryam Azimli <maryam.azimli@ug.bilkent.edu.tr>
2024-12-19 16:03:08 +0300

    connected login with backend

--- client/app/signin/page.tsx
+++ client/app/signin/page.tsx
@@ -19,7 +19,7 @@ export default function SigninPage() {
     setForgotMessage(null);
 
     try {
-      const response = await fetch("/api/signin", {
+        const response = await fetch("http://127.0.0.1:8000/login/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
