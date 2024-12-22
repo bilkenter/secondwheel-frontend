1d1ddf390eeb32dc2c9b5d15e1b32c510c6b208e
Mennatallah Abouelenin <mennatallah@ug.bilkent.edu.tr>
2024-12-22 06:09:31 +0300

    edit vehicle page

--- client/app/vehicle/page.tsx
+++ client/app/vehicle/page.tsx
@@ -29,7 +29,6 @@ interface Vehicle {
   has_sliding_door?: boolean; // Only for vans
 }
 
-
 interface Ad {
   ad_id: number;
   price: number;
@@ -58,6 +57,7 @@ export default function VehiclePage() {
   const [isOfferModalOpen, setIsOfferModalOpen] = useState(false);
   const [offerAmount, setOfferAmount] = useState("");
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [isOfferPending, setIsOfferPending] = useState(false);
 
   //fetch real data
 
@@ -153,6 +153,7 @@ export default function VehiclePage() {
   const handleOfferSubmit = () => {
     console.log("Offer submitted:", offerAmount);
     setIsOfferModalOpen(false); // Close the offer modal after submitting
+    setIsOfferPending(true);
   };
 
   const handleNextImage = () => {
@@ -250,92 +251,157 @@ export default function VehiclePage() {
             >
               Make Offer
             </Button>
+            {isOfferPending && (
+              <p
+                style={{ color: "red", fontStyle: "italic", marginTop: "10px" }}
+              >
+                Your offer is pending approval!
+              </p>
+            )}
           </div>
         </div>
 
         {/* Right Side: Details Card */}
         <Card
-  style={{
-    position: "relative",
-    backgroundColor: "#CDD2EF",
-    border: "1px solid #12314E",
-    boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
-    width: "575px", // Set width to 575px
-    height: "705px", // Set height to 705px
-    marginTop: "20px",
-    marginLeft: "80px",
-  }}
->
-{ad.status.toLowerCase() === "sold" && (
-  <img
-    src="/icons/sold.svg" // Replace with the actual path to your sold.svg file
-    alt="Sold"
-    style={{
-      position: "absolute",
-      top: "0px",
-      right: "-25px",
-      width: "75x", // Adjust width as needed
-      height: "75px", // Adjust height as needed
-      zIndex: 1, // Ensure it appears above other elements
-    }}
-  />
-)}
-  <CardContent
-    className="text-[#12314E]"
-    style={{
-      fontSize: "20px",
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "space-evenly", // Evenly distribute the content vertically
-      textAlign: "center", // Center text within the content
-      height: "100%", // Make sure the CardContent takes full height of the card
-    }}
-  >
-    <p className="mb-2 mt-8"><strong>Vehicle Type:</strong> {vehicle.vehicle_type}</p>
-    <p className="mb-2"><strong>Brand:</strong> {vehicle.brand}</p>
-    <p className="mb-2"><strong>Model Name:</strong> {vehicle.model_name}</p>
-    <p className="mb-2"><strong>Year:</strong> {vehicle.year}</p>
-    <hr className="my-2 border-[#12314E]" />
-    <p className="mb-2"><strong>Mileage:</strong> {vehicle.mileage.toLocaleString()} miles</p>
-    <p className="mb-2"><strong>Fuel Type:</strong> {vehicle.fuel_type}</p>
-    <p className="mb-2"><strong>Fuel Tank Capacity:</strong> {vehicle.fuel_tank_capacity} L</p>
-    <p className="mb-2"><strong>Motor Power:</strong> {vehicle.motor_power} HP</p>
-    <p className="mb-2"><strong>Transmission Type:</strong> {vehicle.transmission_type}</p>
-    <p className="mb-2"><strong>Body Type:</strong> {vehicle.body_type}</p>
-    <p className="mb-2"><strong>Color:</strong> {vehicle.color}</p>
+          style={{
+            position: "relative",
+            backgroundColor: "#CDD2EF",
+            border: "1px solid #12314E",
+            boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
+            width: "575px", // Set width to 575px
+            height: "705px", // Set height to 705px
+            marginTop: "20px",
+            marginLeft: "80px",
+          }}
+        >
+          {ad.status.toLowerCase() === "sold" && (
+            <img
+              src="/icons/sold.svg" // Replace with the actual path to your sold.svg file
+              alt="Sold"
+              style={{
+                position: "absolute",
+                top: "0px",
+                right: "-25px",
+                width: "75x", // Adjust width as needed
+                height: "75px", // Adjust height as needed
+                zIndex: 1, // Ensure it appears above other elements
+              }}
+            />
+          )}
+          <CardContent
+            className="text-[#12314E]"
+            style={{
+              fontSize: "20px",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-evenly", // Evenly distribute the content vertically
+              textAlign: "center", // Center text within the content
+              height: "100%", // Make sure the CardContent takes full height of the card
+            }}
+          >
+            <p className="mb-2 mt-8">
+              <strong>Vehicle Type:</strong> {vehicle.vehicle_type}
+            </p>
+            <p className="mb-2">
+              <strong>Brand:</strong> {vehicle.brand}
+            </p>
+            <p className="mb-2">
+              <strong>Model Name:</strong> {vehicle.model_name}
+            </p>
+            <p className="mb-2">
+              <strong>Year:</strong> {vehicle.year}
+            </p>
+            <hr className="my-2 border-[#12314E]" />
+            <p className="mb-2">
+              <strong>Mileage:</strong> {vehicle.mileage.toLocaleString()} miles
+            </p>
+            <p className="mb-2">
+              <strong>Fuel Type:</strong> {vehicle.fuel_type}
+            </p>
+            <p className="mb-2">
+              <strong>Fuel Tank Capacity:</strong> {vehicle.fuel_tank_capacity}{" "}
+              L
+            </p>
+            <p className="mb-2">
+              <strong>Motor Power:</strong> {vehicle.motor_power} HP
+            </p>
+            <p className="mb-2">
+              <strong>Transmission Type:</strong> {vehicle.transmission_type}
+            </p>
+            <p className="mb-2">
+              <strong>Body Type:</strong> {vehicle.body_type}
+            </p>
+            <p className="mb-2">
+              <strong>Color:</strong> {vehicle.color}
+            </p>
 
-    {/* Conditional fields based on vehicle type */}
-    {vehicle.vehicle_type === "Car" && vehicle.number_of_doors && (
-      <p className="mb-2"><strong>Number of Doors:</strong> {vehicle.number_of_doors}</p>
-    )}
-    {vehicle.vehicle_type === "Motorcycle" && vehicle.wheel_number && (
-      <p className="mb-2"><strong>Wheel Number:</strong> {vehicle.wheel_number}</p>
-    )}
-    {vehicle.vehicle_type === "Motorcycle" && vehicle.cylinder_volume && (
-      <p className="mb-2"><strong>Cylinder Volume:</strong> {vehicle.cylinder_volume} cc</p>
-    )}
-    {vehicle.vehicle_type === "Motorcycle" && vehicle.has_basket !== undefined && (
-      <p className="mb-2"><strong>Has Basket:</strong> {vehicle.has_basket ? "Yes" : "No"}</p>
-    )}
-    {vehicle.vehicle_type === "Van" && vehicle.seat_number && (
-      <p className="mb-2"><strong>Seat Number:</strong> {vehicle.seat_number}</p>
-    )}
-    {vehicle.vehicle_type === "Van" && vehicle.roof_height && (
-      <p className="mb-2"><strong>Roof Height:</strong> {vehicle.roof_height} cm</p>
-    )}
-    {vehicle.vehicle_type === "Van" && vehicle.cabin_space && (
-      <p className="mb-2"><strong>Cabin Space:</strong> {vehicle.cabin_space} m²</p>
-    )}
-    {vehicle.vehicle_type === "Van" && vehicle.has_sliding_door !== undefined && (
-      <p className="mb-2"><strong>Has Sliding Door:</strong> {vehicle.has_sliding_door ? "Yes" : "No"}</p>
-    )}
+            {/* Conditional fields based on vehicle type */}
+            {vehicle.vehicle_type === "Car" && vehicle.number_of_doors && (
+              <p className="mb-2">
+                <strong>Number of Doors:</strong> {vehicle.number_of_doors}
+              </p>
+            )}
+            {vehicle.vehicle_type === "Motorcycle" && vehicle.wheel_number && (
+              <p className="mb-2">
+                <strong>Wheel Number:</strong> {vehicle.wheel_number}
+              </p>
+            )}
+            {vehicle.vehicle_type === "Motorcycle" &&
+              vehicle.cylinder_volume && (
+                <p className="mb-2">
+                  <strong>Cylinder Volume:</strong> {vehicle.cylinder_volume} cc
+                </p>
+              )}
+            {vehicle.vehicle_type === "Motorcycle" &&
+              vehicle.has_basket !== undefined && (
+                <p className="mb-2">
+                  <strong>Has Basket:</strong>{" "}
+                  {vehicle.has_basket ? "Yes" : "No"}
+                </p>
+              )}
+            {vehicle.vehicle_type === "Van" && vehicle.seat_number && (
+              <p className="mb-2">
+                <strong>Seat Number:</strong> {vehicle.seat_number}
+              </p>
+            )}
+            {vehicle.vehicle_type === "Van" && vehicle.roof_height && (
+              <p className="mb-2">
+                <strong>Roof Height:</strong> {vehicle.roof_height} cm
+              </p>
+            )}
+            {vehicle.vehicle_type === "Van" && vehicle.cabin_space && (
+              <p className="mb-2">
+                <strong>Cabin Space:</strong> {vehicle.cabin_space} m²
+              </p>
+            )}
+            {vehicle.vehicle_type === "Van" &&
+              vehicle.has_sliding_door !== undefined && (
+                <p className="mb-2">
+                  <strong>Has Sliding Door:</strong>{" "}
+                  {vehicle.has_sliding_door ? "Yes" : "No"}
+                </p>
+              )}
 
-    <hr className="my-2 border-[#12314E]" />
-    <p className="mb-2"><strong>Location:</strong> {ad.location}</p>
-    <p className="mb-2"><strong>Price:</strong> ${ad.price.toLocaleString()}</p>
-    <p className="mb-2"><strong>Status:</strong> <span style={{ color: ad.status.toLowerCase() === "available" ? "green" : "red" }}>{ad.status}</span></p>
-  </CardContent>
-</Card>
+            <hr className="my-2 border-[#12314E]" />
+            <p className="mb-2">
+              <strong>Location:</strong> {ad.location}
+            </p>
+            <p className="mb-2">
+              <strong>Price:</strong> ${ad.price.toLocaleString()}
+            </p>
+            <p className="mb-2">
+              <strong>Status:</strong>{" "}
+              <span
+                style={{
+                  color:
+                    ad.status.toLowerCase() === "available" ? "green" : "red",
+                }}
+              >
+                {ad.status}
+              </span>
+            </p>
+          </CardContent>
+        </Card>
       </div>
       {/* Contact Seller Modal */}
       {isContactModalOpen && (
@@ -344,40 +410,47 @@ export default function VehiclePage() {
             className="bg-white w-[400px] p-6 rounded-lg shadow-lg"
             style={{ zIndex: 100 }}
           >
-            <h2 className="text-2xl font-bold text-[#12314E] text-center mb-4">Contact Seller</h2>
+            <h2 className="text-2xl font-bold text-[#12314E] text-center mb-4">
+              Contact Seller
+            </h2>
             <div className="text-center">
               <p className="mb-2">Seller Name: {ad.seller_name}</p>
-              <p className="mb-4">Email: <a
-                href={`mailto:${ad.seller_email}`}
-                className="text-[#12314E] underline"
-              >
-                {ad.seller_email}
-              </a></p>
-              <Button
-                  style={{
-                    backgroundColor: "#3A3A3A",
-                    color: "white",
-                    width: "100%",
-                    height: "50px",
-                    fontSize: "16px",
-                    borderRadius: "10px",
-                  }}
-                  onClick={handleCloseModal}
+              <p className="mb-4">
+                Email:{" "}
+                <a
+                  href={`mailto:${ad.seller_email}`}
+                  className="text-[#12314E] underline"
                 >
-                  Cancel
-                </Button>
+                  {ad.seller_email}
+                </a>
+              </p>
+              <Button
+                style={{
+                  backgroundColor: "#3A3A3A",
+                  color: "white",
+                  width: "100%",
+                  height: "50px",
+                  fontSize: "16px",
+                  borderRadius: "10px",
+                }}
+                onClick={handleCloseModal}
+              >
+                Cancel
+              </Button>
             </div>
           </div>
         </div>
       )}
-            {/* Modal for Making an Offer */}
+      {/* Modal for Making an Offer */}
       {isOfferModalOpen && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
           <div
             className="bg-white w-[400px] p-6 rounded-lg shadow-lg"
             style={{ zIndex: 100 }}
           >
-            <h2 className="text-2xl font-bold text-[#3A3A3A] text-center mb-4">Make an Offer</h2>
+            <h2 className="text-2xl font-bold text-[#3A3A3A] text-center mb-4">
+              Make an Offer
+            </h2>
             <div className="flex flex-col items-center">
               <input
                 type="number"
@@ -386,7 +459,9 @@ export default function VehiclePage() {
                 onChange={(e) => setOfferAmount(e.target.value)}
                 className="w-full p-3 rounded-lg border border-[#6C6B6B] mb-1"
               />
-              <small className="italic text-gray-500 mb-4">Your offer will be sent to the seller for review.</small>
+              <small className="italic text-gray-500 mb-4">
+                Your offer will be sent to the seller for review.
+              </small>
               <div className="flex flex-col gap-4">
                 <Button
                   style={{
